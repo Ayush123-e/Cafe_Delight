1cca6218af06a2f857cec5ba720cd89ce056212a
Ayush Kumar <ayushnbt94@gmail.com>
2026-02-20 19:20:10 +0530

    feat: add React app entry point and root App component with routing

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Menu from './pages/Menu'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }, [pathname])
+  return null
+}
+
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App
